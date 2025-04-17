c99b70fe60f540dbd15e9b2b41d96e12f2dbeefa
jhenriquedsm <jhsilvamata@gmail.com>
2025-04-17 16:22:49 -0300

    add ParceriaRepository

---
+++ src/main/java/com/jhenriquedsm/parcerias_serpro/repositories/ParceriaRepository.java
@@ -0,0 +1,8 @@
+package com.jhenriquedsm.parcerias_serpro.repositories;
+
+import com.jhenriquedsm.parcerias_serpro.model.Parceria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParceriaRepository extends JpaRepository<Parceria, Long> {
+    
+}
