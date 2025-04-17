2f7a4ea6a09e23b4047d00762896d204ae645b86
jhenriquedsm <jhsilvamata@gmail.com>
2025-04-17 15:59:47 -0300

    start project

---
+++ src/main/java/com/jhenriquedsm/parcerias_serpro/ParceriasSerproApplication.java
@@ -0,0 +1,13 @@
+package com.jhenriquedsm.parcerias_serpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParceriasSerproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParceriasSerproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhenriquedsm/parcerias_serpro/ParceriasSerproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhenriquedsm.parcerias_serpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParceriasSerproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
