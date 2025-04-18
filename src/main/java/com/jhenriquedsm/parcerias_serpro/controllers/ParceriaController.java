3b68e3117cb1dad07e2bcb0676df9050ef1387a6
jhenriquedsm <jhsilvamata@gmail.com>
2025-04-18 13:16:08 -0300

    add springdoc OpenAPI (Swagger)

--- src/main/java/com/jhenriquedsm/parcerias_serpro/controllers/ParceriaController.java
+++ src/main/java/com/jhenriquedsm/parcerias_serpro/controllers/ParceriaController.java
@@ -2,6 +2,7 @@ package com.jhenriquedsm.parcerias_serpro.controllers;
 
 import com.jhenriquedsm.parcerias_serpro.model.Parceria;
 import com.jhenriquedsm.parcerias_serpro.services.ParceriaService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +18,19 @@ public class ParceriaController {
     ParceriaService parceriaService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Cria uma parceria", description = "Cria uma parceria recebendo uma parceria pelo JSON")
     public Parceria create(@RequestBody Parceria parceria) {
         return parceriaService.create(parceria);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Busca todas as parcerias", description = "Busca todas as parcerias persistidas no banco de dados")
     public List<Parceria> findAll() {
         return parceriaService.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Busca uma parceria", description = "Busca uma parceria pelo id")
     public ResponseEntity<Parceria> findById(@PathVariable(value = "id") Long id) {
         try {
             return ResponseEntity.ok(parceriaService.findById(id));
@@ -36,6 +40,7 @@ public class ParceriaController {
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Atualiza uma parceria", description = "Atualiza os dados de uma parceria existente pelo ID")
     public ResponseEntity<Parceria> update(@PathVariable(value = "id") Long id, @RequestBody Parceria parceria) {
         try {
             parceria.setId(id);
@@ -48,6 +53,7 @@ public class ParceriaController {
 
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deleta uma parceria", description = "Deleta uma parceria pelo id")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         try {
             parceriaService.delete(id);
